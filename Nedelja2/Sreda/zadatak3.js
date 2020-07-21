1fa69e0487b6c6801bdc6dfab5fab2e8f910ec34
Duccaa <gojicduca@yahoo.com>
2020-07-21 22:09:56 +0200

    urađeni zadaci 3 i 4

---
+++ Nedelja2/Sreda/zadatak3.js
@@ -0,0 +1,6 @@
+let num = 58
+if (num % 3 === 0) {
+    console.log(`Broj ${num} je deljiv sa tri`)
+} else {
+    console.log(`Broj ${num} nije deljiv sa tri`)
+}
---
+++ Nedelja2/Sreda/zadatak4.js
@@ -0,0 +1,13 @@
+let f = 1
+let l = 1000
+for(; f <= l; f++){
+    if(f % 5 === 0){
+        console.log(f)
+    }
+}
+while(f <= l){
+    if(f % 5 === 0){
+        console.log(f)
+    }
+    f++
+}
