73d7012f4d2ec17d2e6368a80d42a93dc56ead0a
Duccaa <gojicduca@yahoo.com>
2020-07-29 12:31:09 +0200

    uradjeni zadaci za sredu

---
+++ Nedelja3/Sreda/zadatak1.js
@@ -0,0 +1,14 @@
+// Написати функцију која враћа максимум од 3 броја (Параметри функције су 3 броја)
+
+function max(x, y, z) {
+    return Math.max(x, y, z);
+}
+
+max(3, -150, 50)
+
+
+function isDivisibleBy3(num) {
+    return num % 3 === 0;
+}
+
+console.log(isDivisibleBy3(6));
---
+++ Nedelja3/Sreda/zadatak2.js
@@ -0,0 +1,8 @@
+// Написати функцију која израчунава цену пице по квадратном центиметру (параметри функције су полупречник у цм, и цена пице)
+
+
+function cenaPoKvadratnomCentimetru(r, cena) {
+    console.log(Math.pow(r, r) * Math.PI / cena);
+}
+
+cenaPoKvadratnomCentimetru(20, 900);
