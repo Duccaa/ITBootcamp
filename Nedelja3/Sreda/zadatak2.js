02c742eba16c48dc37f44b250f97392db052626f
Duccaa <gojicduca@yahoo.com>
2020-07-29 22:38:34 +0200

    zadatak2 update

--- Nedelja3/Sreda/zadatak2.js
+++ Nedelja3/Sreda/zadatak2.js
@@ -1,11 +1,17 @@
 // Написати функцију која израчунава цену пице по квадратном центиметру (параметри функције су полупречник у цм, и цена пице)
 
-function cenaPoKvadratnomCentimetru(r, cena) {
-    if(r > 0 && cena > 0) {
-        console.log(cena / (Math.pow(r, r) * Math.PI));
-    } else {
-        console.log('Neispravno uneti parametri')
-    } 
-}
+// function cenaPoKvadratnomCentimetru(r, cena) {
+//     if(r > 0 && cena > 0) {
+//         console.log(cena / (Math.pow(r, 2) * Math.PI));
+//     } else {
+//         console.log('Neispravno uneti parametri')
+//     } 
+// }
 
+function cenaPoKvadratnomCentimetru(r, cena) {
+    r > 0 && cena > 0 ?
+    console.log(cena / (Math.pow(r, 2) * Math.PI))
+    :
+    console.log('Neispravno uneti parametri')
+}
 cenaPoKvadratnomCentimetru(20, 800);
