9c25cf896029fe13aba8f746224f051b7f2f1603
Duccaa <gojicduca@yahoo.com>
2020-07-29 16:38:02 +0200

    domaci

--- Nedelja3/Sreda/zadatak1.js
+++ Nedelja3/Sreda/zadatak1.js
@@ -1,8 +1,19 @@
-// Написати функцију која враћа максимум од 3 броја (Параметри функције су 3 броја)
+// Написати функцију која враћа максимум од 3 броја (параметри функције су 3 броја)
 
-function max(x, y, z) {
-    return Math.max(x, y, z);
+// function maxValue(x, y, z) {
+//     return Math.max(x, y, z);
+// }
+// console.log(maxValue(3, -150, 50));
+
+function maxValue(x, y, z) {
+    if(x > y && x > z) {
+        return x;
+    } else if (y > x && y > z) {
+        return y;
+    } else {
+        return z;
+    }
 }
+console.log(maxValue(3, -150, 50))
 
-console.log(max(3, -150, 50));
-
+//Jos uvek nisam skapirala kako da uradim sa Math.min, kao sto je Cvijan rekao ...
--- Nedelja3/Sreda/zadatak2.js
+++ Nedelja3/Sreda/zadatak2.js
@@ -1,7 +1,11 @@
 // Написати функцију која израчунава цену пице по квадратном центиметру (параметри функције су полупречник у цм, и цена пице)
 
 function cenaPoKvadratnomCentimetru(r, cena) {
-    console.log(Math.pow(r, r) * Math.PI / cena);
+    if(r > 0 && cena > 0) {
+        console.log(cena / (Math.pow(r, r) * Math.PI));
+    } else {
+        console.log('Neispravno uneti parametri')
+    } 
 }
 
-cenaPoKvadratnomCentimetru(20, 900);
+cenaPoKvadratnomCentimetru(20, 800);
