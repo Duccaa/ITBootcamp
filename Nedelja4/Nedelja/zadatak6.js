54434c8b092ee51e1a9171f1c98015d655fc6055
Duccaa <gojicduca@yahoo.com>
2020-08-09 00:09:44 +0200

    zadatak6 update

--- Nedelja4/Nedelja/zadatak6.js
+++ Nedelja4/Nedelja/zadatak6.js
@@ -1,6 +1,8 @@
-const pizzaList = document.getElementById('order-pizza')
+const pizzaList = document.getElementById('order-pizza');
 const orderButtons = document.getElementById('order');
 const orderNowButtons = document.getElementById('order-now');
+const firstP = document.getElementById('first-paragraph');
+const outputDiv = document.getElementById('output');
 
 let X = 0;
 pizzaList.addEventListener('click', count) 
@@ -10,24 +12,25 @@ function count(e){
     if(e.target.classList.contains('order')) {
         X++;
         console.log(X);
-        let p = document.createElement('p')
-        p.textContent = `Број пица за поручивање: ${X}`;
-        pizzaList.appendChild(p);
+        // let p = document.createElement('p')
+        firstP.textContent = `Број пица за поручивање: ${X}`;
+        // pizzaList.appendChild(p);
     }   
 }
 
 function reset(e) {
     if(e.target.classList.contains('order-now')) {
+        console.log(e.targetElment)
         let p = document.createElement('p')
         if(X === 0) {
             p.textContent = 'Грешка';
-            pizzaList.appendChild(p);
+            outputDiv.appendChild(p);
             setTimeout(() => {
                 p.remove()
             },1200)
         } else {
-            p.textContent = `Наручили сте ${X} пице, ${(new Date()).getHours()}:${(new Date()).getMinutes()}:${(new Date()).getSeconds()}.`;
-            pizzaList.appendChild(p);
+            p.textContent = `Наручили сте ${X} пица, ${(new Date()).getHours()}:${(new Date()).getMinutes()}:${(new Date()).getSeconds()}.`;
+            outputDiv.appendChild(p);
             X = 0;
             console.log(X);
         } 
