1e191a26505696f973f744d0215aa9b150cc2b24
Duccaa <62803772+Duccaa@users.noreply.github.com>
2020-08-16 01:29:57 +0200

    Delete module1.js

--- Nedelja5/src/module1.js
+++
@@ -1,8 +0,0 @@
-export function segregateAbilities(pokemonsSet) {
-    let pokemonsAbilities = []
-    pokemonsSet.forEach(pokemon => {
-    pokemonsAbilities.push(pokemon.abilities)
-    })
-    let concatAbitities = [].concat.apply([], pokemonsAbilities)
-    return concatAbitities 
-}
